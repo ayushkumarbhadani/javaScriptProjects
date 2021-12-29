c0c8bdfbd6f4664740ad5f48bb2cf85f206e467c
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2021-12-30 00:13:50 +0530

    Improved Functionality of edit and delete buttons

--- todoList/app.js
+++ todoList/app.js
@@ -1,42 +1,31 @@
 let input=document.querySelector("#new-task");
 let form=document.querySelector("#addNewTask");
 let todoList=[];
+let index=-1;
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
+    
     if(!todoList.includes(input.value)){
-        var isEdit=false;
-        if(input.value!=="" && !isEdit){
-            todoList.push(input.value);
+        if(input.value!==""){
+            console.log(`indexinside:${index}`);
+            if(index==-1){
+                todoList.push(input.value);
+            }
+            else{
+                todoList[index]=input.value;
+                index=-1;
+                document.querySelector(".btn").textContent="Add Item";
+            }
             input.value="";
             document.querySelector(".good").innerHTML="Task added Successfully";
             displayNoneToggle(".good");
-            let todoListText=todoList.map((item)=>{
-                return`
-                <div class="todo-list-container">
-                <input type="checkbox">
-                <p>${item}</p>
-                <button class="modify-todo edit"><i class="far fa-edit"></i></button>
-                <button class="modify-todo delete"><i class="far fa-trash-alt"></i></button>
-            </div>
-                `;
-                
-            });
-            todoListText=todoListText.join("");
-            document.querySelector(".todo-list").innerHTML=todoListText;
+            renderTodo();
             addEvent();
-        }
-        else if(isEdit){
-            let temVar=item.previousElementSibling.textContent;
-            input.value=temVar;
-            input.focus();
-            
-            todoList[todoList.indexOf(temVar)]=input.value;
         }
         else{
             document.querySelector(".danger").innerHTML="Please Enter Somthing";
             displayNoneToggle(".danger");
-        }
-        
+        }  
     }
     else{
         console.log("Already Added");
@@ -44,17 +33,45 @@ form.addEventListener("submit",(e)=>{
         displayNoneToggle(".warnings");
     }
     console.log(todoList);
-    
-    
 });
 function displayNoneToggle(item){
     document.querySelector(item).classList.remove("displayNone");
     setTimeout(()=>document.querySelector(item).classList.add("displayNone"),3000);
 }
+function renderTodo(){
+    let todoListText=todoList.map((item)=>{
+        return`
+        <div class="todo-list-container">
+        <input type="checkbox">
+        <p>${item}</p>
+        <button class="modify-todo edit"><i class="far fa-edit"></i></button>
+        <button class="modify-todo delete"><i class="far fa-trash-alt"></i></button>
+    </div>
+        `;
+    });
+    todoListText=todoListText.join("");
+    document.querySelector(".todo-list").innerHTML=todoListText;
+    addEvent();
+}
 function addEvent(){
     document.querySelectorAll(".edit").forEach((item)=>{
         item.addEventListener("click",()=>{
-            isEdit=true;
+            var tempvar=item.previousElementSibling.textContent;
+            input.value=item.previousElementSibling.textContent;
+            input.focus();
+            index=todoList.indexOf(tempvar);
+            document.querySelector(".btn").textContent="Update";
+        });
+    });
+    document.querySelectorAll(".delete").forEach((item)=>{
+        item.addEventListener("click",()=>{
+            var tempvar=item.previousElementSibling.previousElementSibling.textContent;
+            var deleteIndex=todoList.indexOf(tempvar);
+            if(deleteIndex!==-1){
+                todoList.splice(deleteIndex,1);
+            }
+            console.log(`After del: ${todoList}`);
+            renderTodo();
         });
     });
 }
